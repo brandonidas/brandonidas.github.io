8db4d52f11ecdcf83dc0e34ec024f96a2657bcca
Brandon Tong Jia Xin <brandonidas@gmail.com>
2018-12-08 15:49:37 -0800

    Added links for github and ng demo

--- front-demo/app.js
+++ front-demo/app.js
@@ -1,43 +1,6 @@
-var app = angular.module('myApp', ['ngRoute']);
-
-app.config(function($routeProvider) {
-  $routeProvider
-
-  .when('/', {
-    templateUrl : 'pages/home.html',
-    controller  : 'BeerCounter'
-  })
-
-  .when('/blog', {
-    templateUrl : 'pages/blog.html',
-    controller  : 'BlogController'
-  })
-
-  .when('/about', {
-    templateUrl : 'pages/about.html',
-    controller  : 'AboutController'
-  })
-
-  .otherwise({redirectTo: '/'});
-});
-
-app.controller('HomeController', function($scope) {
-  $scope.message = 'Hello from HomeController';
-});
-
-app.controller('BlogController', function($scope) {
-  $scope.message = 'Hello from BlogController';
-});
-
-app.controller('AboutController', function($scope) {
-  $scope.message = 'Hello from AboutController';
-});
-//-----
-
-
 angular.module('app', ['components'])
-app.controller('BeerCounter', function($scope, $locale) {
-  $scope.message = 'Hello from HomeController';
+ 
+.controller('BeerCounter', function($scope, $locale) {
   $scope.beers = [0, 1, 2, 3, 4, 5, 6];
   if ($locale.id == 'en-us') {
     $scope.beerForms = {
