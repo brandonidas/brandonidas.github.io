dc218ac6ec68c3eb7f6b4c998c96bcbcba7051bc
Brandon Tong Jia Xin <brandonidas@gmail.com>
2018-12-10 12:40:36 -0800

    added todo function to ng-demo
    has extra controller to main app.js
    
    app.js refactored accordingly

--- front-demo/app.js
+++ front-demo/app.js
@@ -1,6 +1,6 @@
-angular.module('app', ['components'])
+var app = angular.module('app', ['components']);
  
-.controller('BeerCounter', function($scope, $locale) {
+app.controller('BeerCounter', function($scope, $locale) {
   $scope.beers = [0, 1, 2, 3, 4, 5, 6];
   if ($locale.id == 'en-us') {
     $scope.beerForms = {
@@ -17,3 +17,31 @@ angular.module('app', ['components'])
     };
   }
 });
+
+app.controller('TodoListController', function() {
+    var todoList = this;
+    todoList.todos = [
+      {text:'learn AngularJS', done:true},
+      {text:'build an AngularJS app', done:false}];
+ 
+    todoList.addTodo = function() {
+      todoList.todos.push({text:todoList.todoText, done:false});
+      todoList.todoText = '';
+    };
+ 
+    todoList.remaining = function() {
+      var count = 0;
+      angular.forEach(todoList.todos, function(todo) {
+        count += todo.done ? 0 : 1;
+      });
+      return count;
+    };
+ 
+    todoList.archive = function() {
+      var oldTodos = todoList.todos;
+      todoList.todos = [];
+      angular.forEach(oldTodos, function(todo) {
+        if (!todo.done) todoList.todos.push(todo);
+      });
+    };
+  });
