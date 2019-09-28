1d1efe40f9e7d45dd496db8ecafce4f0c699f4c4
Brandon Tong <brandonidas@gmail.com>
2019-09-28 23:07:05 +0800

    Added tf.js stuff to portfolio

--- demos/linear_prediction.js
+++ demos/linear_prediction.js
@@ -1,14 +1,15 @@
 console.log('Hello TensorFlow');
+
 // y = mx + 0, predict m
-// r is random variance along y axis of data
+// r is random variance along y axis
 // n is number of data points
-function createData(m = 1, r = 3, n = 10) {
+function createData(m = 1, r = 2.3, n = 10) {
     xyObjectArray = [];
     for (i = 0; i < n; i++) {
         yEntropy = r*((Math.random() > 0.5) ? - Math.random() : Math.random())
         var xy = {
             x: i,
-            y: i + yEntropy
+            y: Math.round((i + yEntropy) * 100) / 100
         }
         console.log("y value added:" + yEntropy)
         xyObjectArray.push(xy);
@@ -19,14 +20,15 @@ xyObjectArray = createData();
 
 var ctx = document.getElementById('myChart1').getContext('2d');
 var scatterChart = new Chart(ctx, {
-    type: 'scatter',
-    label: xyObjectArray.map(e => e.x),
+    type: 'line',
+
     data: {
         datasets: [{
             borderColor: 'rgba(54, 162, 235, 0.9)',
             backgroundColor: 'rgba(54, 162, 235, 0.9)',
             label: 'Sample Linear Data',
-            data: xyObjectArray
+            data: xyObjectArray,
+            showLine: false
         }]
     },
     options: {
@@ -46,7 +48,9 @@ function addData(dataXYArray) {
         borderColor: "red",
         backgroundColor: "red",
         label: 'Predicted Data',
-        data: dataXYArray
+        data: dataXYArray,
+        pointRadius: 1,
+        fill:false,
     }
     scatterChart.data.datasets.push(dataset);
     // scatterChart.data.datasets.forEach((dataset) => {
@@ -67,23 +71,26 @@ async function linearPredict() {
     });
 
     xArray = xyObjectArray.map(e => e.x);
-    document.getElementById("xV").innerText = xArray;
-    yArray = xyObjectArray.map(e => Math.round(e.y * 100) / 100);
-    document.getElementById("yV").innerText = yArray;
+    yArray = xyObjectArray.map(e => e.y);
+
+    document.getElementById("xV").innerText = "sample x: " + xArray;
+    document.getElementById("yV").innerText = "sample y: " + yArray;
 
     const xs = tf.tensor2d(xArray, [xArray.length, 1])
     const ys = tf.tensor2d(yArray, [yArray.length, 1])
     await model.fit(xs, ys, { epochs: 100 });
 
-    prediction = model.predict(tf.tensor2d(xArray, [xArray.length, 1]));
-    x = document.getElementById("demo");
-    x.innerText = prediction;
+    // PREDICTION PHASE
+    inputXArray = Array.from(Array(xyObjectArray.length).keys())
+    console.log("inputXarray = "+ inputXArray)
+
+    prediction = model.predict(tf.tensor2d(inputXArray, [inputXArray.length, 1]));
     predictionVal = (prediction.dataSync());
-    console.log(predictionVal)
-    predictedData = Array(xyObjectArray.length);
+    // document.getElementById("demo").innerText = "predicted-y: " + predictionVal;
+    predictedData = Array(inputXArray.length);
     for (let index = 0; index < predictedData.length; index++) {
         predictedData[index] = {
-            x: xArray[index],
+            x: inputXArray[index],
             y: predictionVal[index]
         }
     }
@@ -91,4 +98,3 @@ async function linearPredict() {
     addData(predictedData)
 
 }
-linearPredict()
