72e4683f5cc54e26d42dd89b6f7d1dc3b5b81f30
Brandon Tong <brandonidas@gmail.com>
2025-04-20 13:44:32 -0700

    added gh pages specific paths

--- bbt-pwa/service-worker.js
+++ bbt-pwa/service-worker.js
@@ -1,21 +1,21 @@
 const CACHE_NAME = "big-button-timer-v1";
 const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/service-worker.js",
-  "/icons/icon-192.png",
-  "/icons/icon-512.png"
+  "/bbt-pwa/",
+  "/bbt-pwa/index.html",
+  "/bbt-pwa/manifest.json",
+  "/bbt-pwa/service-worker.js",
+  "/bbt-pwa/icons/icon-192.png",
+  "/bbt-pwa/icons/icon-512.png",
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE)),
   );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((res) => res || fetch(event.request))
+    caches.match(event.request).then((res) => res || fetch(event.request)),
   );
 });
